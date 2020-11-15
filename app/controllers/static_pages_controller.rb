eea8be5f75bfceb830586246fe7abd2ef89ceffa
Natashia White <whitenat@oregonstate.edu>
2020-11-15 10:18:55 -0800

    add business name filter to applications query

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,14 +2,13 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    @companies = Company.where(business_name: params[:business_name]) || Company.all
-    @applications = Application.where(jobs_retained: params[:jobs_retained]) || Application.all
+    @companies = Company.all
+    @applications = Application.where(jobs_retained: params[:jobs_retained], business_name: params[:business_name]) || Application.all
     @owners = Owner.all
   end
 
   def new_query_params
-  	params.require(:applications).permit(:jobs_retained)
-  	params.require(:company).permit(:business_name)
+  	params.require(:applications).permit(:jobs_retained, :business_name)
 	end
 
   def browse; end

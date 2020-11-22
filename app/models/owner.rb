e0cd9ddc6436f4c8aa0ed9e7abe263cf9bbfbc8c
Wyatt Pierce <wyattp23@gmail.com>
2020-11-21 22:50:34 -0800

    Make adjustments for new spreadsheet layouts.

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -11,21 +11,27 @@ class Owner < ApplicationRecord
 
   def self.import(demographic_file)
     CSV.foreach(demographic_file.path, headers: true) do |row|
-        o = Owner.new
-        o.ein = row["Employer Identification Number (Federal EIN)"] || "n/a"
-        o.name = row["Name"] || "n/a"
-        o.percent_ownership = row["% Ownership"] || "n/a"
-        o.race = row["Race"] || "n/a"
-        o.ethnicity = row["Ethnicity"] || "n/a"
-        o.gender = row["Gender"] || "n/a"
-        o.save
+        max_owners = 3
 
-        companies = Company.where(ein: o.ein)
-        companies.each do |c|
-          c.owners << o
-          # I'm not sure if this is needed, I'm getting duplicate
-          #values when including it
-          # o.companies << c
+        max_owners.times do |i|
+          if row["Business Owner Name #{i}"]
+            o = Owner.new
+            o.ein = row["Employer Identification Number (Federal EIN)"] || "n/a"
+            o.name = row["Business Owner Name #{i}"] || "n/a"
+            o.percent_ownership = row["% Ownership #{i}"] || "n/a"
+            o.race = row["Race #{i}"] || "n/a"
+            o.ethnicity = row["Ethnicity #{i}"] || "n/a"
+            o.gender = row["Gender #{i}"] || "n/a"
+            o.save
+
+            companies = Company.where(ein: o.ein)
+            companies.each do |c|
+              c.owners << o
+              # I'm not sure if this is needed, I'm getting duplicate
+              #values when including it
+              # o.companies << c
+            end
+          end
         end
 
       end

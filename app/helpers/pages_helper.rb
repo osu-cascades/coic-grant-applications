bbb270e853b45b9354dea1641df110b6eb523145
Wyatt Pierce <wyattp23@gmail.com>
2020-11-21 19:33:15 -0800

    Remove trailing commas from results table.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,8 +2,11 @@ module PagesHelper
 
   def get_owner_race(owners)
     owner_races = ''
-    owners.each do |o|
-      owner_races += o.race + ", "
+    owners.each_with_index do |owner, index|
+      owner_races += owner.race
+      if index < owners.length() - 1
+        owner_races += ", "
+      end
     end
 
     return owner_races
@@ -11,19 +14,25 @@ module PagesHelper
 
 	def get_owner_ethnicity(owners)
 		owner_ethnicities = ''
-		owners.each do |o|
-			owner_ethnicities += o.ethnicity + ", "
+		owners.each_with_index do |owner, index|
+      owner_ethnicities += owner.ethnicity
+      if index < owners.length() - 1
+        owner_ethnicities += ", "
+      end
 		end
 
 		return owner_ethnicities
 	end
   
 	def get_owner_gender(owners)
-		owner_gender = ''
-		owners.each do |o|
-			owner_gender += o.gender + ", "
+		owner_genders = ''
+		owners.each_with_index do |owner, index|
+      owner_genders += owner.gender
+      if index < owners.length() - 1
+        owner_genders += ", "
+      end
 		end
 
-		return owner_gender
+		return owner_genders
 	end
 end

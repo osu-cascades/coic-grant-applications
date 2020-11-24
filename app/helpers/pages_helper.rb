f294c25a164a2b50abe8d8ce69a9b09af5b6dc50
Wyatt Pierce <wyattp23@gmail.com>
2020-11-23 22:05:25 -0800

    Add functionality to update amount awarded.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -11,4 +11,15 @@ module PagesHelper
 
     return owner_attributes
   end
+
+  def sum_award_amounts(applications)
+    sum = 0
+    applications.each do |app|
+      if app.amount_approved != "n/a"
+        sum += app.amount_approved.delete(",$").to_f
+      end
+    end
+
+    return sum.round(2).to_s
+  end
 end

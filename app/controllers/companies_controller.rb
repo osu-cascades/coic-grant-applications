d320f18adbe0145334c6a31fb7525fcf89850934
Wyatt Pierce <wyattp23@gmail.com>
2020-12-01 19:33:19 -0800

    Add destroy functionality for companies.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -54,6 +54,8 @@ class CompaniesController < ApplicationController
   # DELETE /companies/1
   # DELETE /companies/1.json
   def destroy
+    Application.where(ein: @company.ein).destroy_all
+
     @company.destroy
     respond_to do |format|
       format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }

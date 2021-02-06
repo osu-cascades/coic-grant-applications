c93afe9455674fdb0755656292ccf97d64b1f1d3
Wyatt Pierce <wyattp23@gmail.com>
2021-02-06 14:13:14 -0800

    Remove unused helper function.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,17 +1,5 @@
 module PagesHelper
 
-  def get_owner_attributes(owners, attr)
-    owner_attributes = ''
-    owners.each_with_index do |owner, index|
-      owner_attributes += owner.send(attr)
-      if index < owners.length() - 1
-        owner_attributes += ", "
-      end
-    end
-
-    return owner_attributes
-  end
-
   def sum_award_amounts(applications)
     sum = 0
     applications.each do |app|

77100e5ff3b0587abb19012b4b5ea9a05d913105
Wyatt Pierce <wyattp23@gmail.com>
2021-03-11 15:38:35 -0800

    Remove CSV exporting from application model.

--- app/models/application.rb
+++ app/models/application.rb
@@ -21,27 +21,4 @@ class Application < ApplicationRecord
     end
   end
 
-  # def self.to_csv
-  #   attributes = %w{round business_name business_size business_type jobs_retained ein bin naics zip county city}
-
-  #   CSV.generate(headers: true) do |csv|
-  #     csv << attributes
-
-  #     all.find_each do |user|
-  #       csv << attributes.map{ |attr| query.send(attr) }
-  #     end
-  #   end
-  # end
-  # def self.to_csv
-  #   column_names = %w{round business_name business_size business_type jobs_retained ein bin naics zip county city}
-    
-  #   CSV.generate(headers: true) do |csv|
-  #     csv << column_names
-  #     all.find_each do |application|
-  #       #csv << result.attributes.values_at(*column_names)
-  #       csv << column_names.map{ |attr| application.send(attr) }
-  #     end
-  #   end
-  # end
-
 end

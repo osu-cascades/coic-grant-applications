849d3a46bd94c5223b0b1f72e5d0086b23e061f5
Wyatt Pierce <wyattp23@gmail.com>
2021-04-08 21:02:58 -0700

    Initialize company by business name.

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,8 +5,9 @@ class Company < ApplicationRecord
 
   def self.import(round_file)
     CSV.foreach(round_file.path, headers: true) do |row|
-      c = Company.find_or_initialize_by(ein: row["Employer Identification Number (Federal EIN)"])
-      c.business_name = row["Business Name"] || "n/a"
+      #c = Company.find_or_initialize_by(ein: row["Employer Identification Number (Federal EIN)"])
+      c = Company.find_or_initialize_by(business_name: row["Business Name"])
+      c.ein = row["Employer Identification Number (Federal EIN)"] || "n/a"
       c.bin = row["Business Identification Number (BIN issued by Oregon Employment Department)"] || "n/a"
       c.naics = row["NAICS"] || "n/a"
       c.zip = row["Zip Code"] || "n/a"

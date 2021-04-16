d2595b73f769ba69cec64a2118359bf71e8f3b7d
Natashia White <whitenat@oregonstate.edu>
2021-04-16 09:14:01 -0700

    fix form formatting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_170726) do
+ActiveRecord::Schema.define(version: 2021_04_14_040958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2021_04_09_170726) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ein"
+    t.string "business_name"
   end
 
   create_table "queries", force: :cascade do |t|

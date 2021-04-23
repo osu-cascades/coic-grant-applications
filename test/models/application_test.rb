5c5aa92949b1b192f7e65bdbafd325f35e355639
Wyatt Pierce <wyattp23@gmail.com>
2021-04-22 19:36:42 -0700

    Add valid application test.

--- test/models/application_test.rb
+++ test/models/application_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ApplicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid application' do
+    a = Application.new(
+      round: "test",
+      jobs_retained: "test",
+      amount_approved: "test",
+    )
+
+    assert a.valid?
+  end
 end

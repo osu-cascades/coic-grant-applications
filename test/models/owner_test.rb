a2b15cd5226aa7d50ca5cfc15f6184de6494c0a3
Wyatt Pierce <wyattp23@gmail.com>
2021-04-22 19:43:29 -0700

    Add valid owner test.

--- test/models/owner_test.rb
+++ test/models/owner_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class OwnerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid owner" do
+    o = Owner.new(
+      name: "test",
+      percent_ownership: "test",
+      race: "test",
+      ethnicity: "test",
+      gender: "test"
+    )
+
+    assert o.valid?
+  end
 end

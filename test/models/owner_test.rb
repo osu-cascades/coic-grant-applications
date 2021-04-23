a1bba97757b96896cfb9c1f9532f731eb9585cf7
Wyatt Pierce <wyattp23@gmail.com>
2021-04-22 19:47:16 -0700

    Add invalid owner tests.

--- test/models/owner_test.rb
+++ test/models/owner_test.rb
@@ -12,4 +12,24 @@ class OwnerTest < ActiveSupport::TestCase
 
     assert o.valid?
   end
+
+  test "invalid without name" do
+    o = Owner.new(
+      percent_ownership: "test",
+      race: "test",
+      ethnicity: "test",
+      gender: "test"
+    )
+
+    refute o.valid?
+  end
+
+  test "invalid without demographic data" do
+    o = Owner.new(
+      name: "test",
+      percent_ownership: "test"
+    )
+
+    refute o.valid?
+  end
 end

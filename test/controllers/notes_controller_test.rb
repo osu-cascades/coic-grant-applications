5361139ca3ab78addde8d2ca45a118ce9ff81014
Wyatt Pierce <wyattp23@gmail.com>
2021-04-29 16:35:11 -0700

    Add user sign in to notes controller tests.

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class NotesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    get '/users/sign_in'
+    sign_in users(:admin)
+    post user_session_url
+  end
+
   test "should get index" do
     get notes_index_url
     assert_response :success

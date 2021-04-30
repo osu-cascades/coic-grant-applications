7f133e688a08dbd7943580ebf2d18ae205e34359
Wyatt Pierce <wyattp23@gmail.com>
2021-04-29 21:21:59 -0700

    Use test file in create upload controller test (WIP)

--- test/controllers/uploads_controller_test.rb
+++ test/controllers/uploads_controller_test.rb
@@ -22,7 +22,7 @@ class UploadsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create upload" do
     assert_difference('Upload.count') do
-      post uploads_url, params: { upload: {  } }
+      post uploads_url, params: { upload: { round: "test", data: file_fixture("test.csv").read } }
     end
 
     assert_redirected_to upload_url(Upload.last)

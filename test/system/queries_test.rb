b8a6fd367428410db5a4a5aa22fb716190b377a8
Wyatt Pierce <wyattp23@gmail.com>
2021-05-13 18:19:57 -0700

    Remove unnecessary tests for query.

--- test/system/queries_test.rb
+++ test/system/queries_test.rb
@@ -24,22 +24,4 @@ class QueriesTest < ApplicationSystemTestCase
     click_on "Back"
   end
 
-  test "updating a Query" do
-    visit queries_url
-    click_on "Edit", match: :first
-
-    click_on "Update Query"
-
-    assert_text "Query was successfully updated"
-    click_on "Back"
-  end
-
-  test "destroying a Query" do
-    visit queries_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Query was successfully destroyed"
-  end
 end
